4ae049dfb81bb9084b421ec20e8b9f900e572e33
idilia wheat <lillyw54@gmail.com>
2023-10-24 16:16:32 -0500

    update on questions

---
+++ Develop/README.md
@@ -0,0 +1,8 @@
+
+# read me file
+
+## Description
+creating a read me file through node.js
+
+## Installation
+just run npm install
--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,61 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+var fs = require("fs")
+
+inquirer
+  .prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+    }
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description?"
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+
+var template = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Installation
+just run npm install`
+
+// console.log(template)
+
+fs.writeFile("README.md", template, function() {
+    console.log("Successful!")
+})
+
+  })
+
+  
+
+//   project title, 
+
+
+//   description, 
+//   installation instructions, 
+//   usage information, 
+//   contribution guidelines, 
+//   test instructions
+
+
+
+
+
+
+
+
+
+
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -11,3 +68,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
